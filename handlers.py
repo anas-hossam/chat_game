04f78b9c8cb3b1b9198a3f65e952a98451f5ae7d
Al-shimaa Adel <alshimaa.adel11@gmail.com>
2017-03-21 05:45:51 +0200

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -7,8 +7,12 @@ application = tornado.web.Application([
     (r"/login",Users.Login),
     (r"/create" ,Users.SignUp),
     (r"/show",Users.ShowUsers),
-],static_path='scripts',debug=True)
+],static_path='scripts',debug=True,cookie_secret="61oETzKXQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=")
 
 if __name__ == "__main__":
     application.listen(8888)
     tornado.ioloop.IOLoop.instance().start()
+
+
+#(r"/(style\.css)",tornado.web.StaticFileHandler, {"path": "./css/"}),
+#(r"/css/(.*)", tornado.web.StaticFileHandler, {"path": os.path.join(os. path.dirname(__file__), 'css')}),
